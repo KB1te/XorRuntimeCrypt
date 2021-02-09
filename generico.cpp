270a3cedf47ff1a782a33555f27fdc2a3fd93659
KB1te <63610437+KB1te@users.noreply.github.com>
2021-02-08 22:39:00 -0300

    Add files via upload

---
+++ generico.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <Windows.h>
+#include <winternl.h>
+#pragma comment(lib,"ntdll.lib")
+
+const char *Crypt(const char *buff) {
+	PEB peb = { 0 };
+	NtQueryInformationProcess(GetCurrentProcess(), ProcessBasicInformation, &peb, sizeof(PROCESS_BASIC_INFORMATION), NULL);
+	char *out = (char*)calloc(sizeof(buff), sizeof(char*));
+	if (out == nullptr) {
+		return NULL;
+	}
+	for (unsigned int i = 0; i < strlen(buff); i++) {
+		out[i] = buff[i] ^ (DWORD32)&peb;
+	}
+	return out;
+}
+
+int main()
+{
+	const char *toCrypt = Crypt("KB1te");
+	printf("%s\n",toCrypt);
+	printf("%s\n", Crypt(toCrypt));
+}
+
